656b80c54af47cdaee9b831d0def65e846a075fd
hamzakargin <hamzakargin@outlook.com>
2025-07-24 14:37:32 +0300

    Create job form completed

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -10,10 +10,14 @@ import {
   CreateAndEditJobType,
 } from "@/utils/types";
 
-import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-
+import { Button } from "./ui/button";
 import { CustomFormField, CustomFormSelect } from "./FormComponent";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createJobAction } from "@/utils/actions";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 function CreateJobForm() {
   // 1. Define your form.
@@ -27,18 +31,33 @@ function CreateJobForm() {
       mode: JobMode.FullTime,
     },
   });
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: (values: CreateAndEditJobType) => createJobAction(values),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error("There was an error");
+        return;
+      }
+      toast("job created");
+      queryClient.invalidateQueries({ queryKey: ["jobs"] });
+      queryClient.invalidateQueries({ queryKey: ["stats"] });
+      queryClient.invalidateQueries({ queryKey: ["charts"] });
+
+      router.push("/jobs");
+      // form.reset();
+    },
+  });
 
   function onSubmit(values: CreateAndEditJobType) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    mutate(values);
   }
-
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit(onSubmit)}
         className="bg-muted p-8 rounded"
+        onSubmit={form.handleSubmit(onSubmit)}
       >
         <h2 className="capitalize font-semibold text-4xl mb-6">add job</h2>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
@@ -48,7 +67,6 @@ function CreateJobForm() {
           <CustomFormField name="company" control={form.control} />
           {/* location */}
           <CustomFormField name="location" control={form.control} />
-
           {/* job status */}
           <CustomFormSelect
             name="status"
@@ -63,9 +81,12 @@ function CreateJobForm() {
             labelText="job mode"
             items={Object.values(JobMode)}
           />
-
-          <Button type="submit" className="self-end capitalize">
-            create job
+          <Button
+            type="submit"
+            className="self-end capitalize"
+            disabled={isPending}
+          >
+            {isPending ? "loading..." : "create job"}
           </Button>
         </div>
       </form>
